ceea8f9395e092adc6603eda7635ab06d647baa1
ko-shiro-hap <koushirou.iino@icloud.com>
2024-12-15 10:16:36 +0900

    add: カスタムシェイプ

--- src/features/canvas/Canvas.tsx
+++ src/features/canvas/Canvas.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useEffect, useState } from 'react';
-import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
+import { Stage, Layer, Rect, Text, Circle, Line, Shape } from 'react-konva';
 
 export default function Canvas() {
   const [isMounted, setIsMounted] = useState(false);
@@ -34,6 +34,27 @@ export default function Canvas() {
           fillLinearGradientEndPoint={{ x: 50, y: 50 }}
           fillLinearGradientColorStops={[0, 'red', 1, 'yellow']}
         />
+        <Shape
+            width={260}
+            height={170}
+            x={280}
+            y={50}
+            sceneFunc={function (context, shape) {
+              const width = shape.width();
+              const height = shape.height();
+              context.beginPath();
+              context.moveTo(0, 0);
+              context.lineTo(width - 40, height - 90);
+              context.quadraticCurveTo(width - 110, height - 70, width, height);
+              context.closePath();
+
+              // (!) Konva specific method, it is very important
+              context.fillStrokeShape(shape);
+            }}
+            fill="#00D2FF"
+            stroke="black"
+            strokeWidth={4}
+          />
       </Layer>
     </Stage>
   ) : (
